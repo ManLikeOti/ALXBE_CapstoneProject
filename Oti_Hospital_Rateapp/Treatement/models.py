895e7863f470add209828b4bfe4e5c05ed1f672d
ManLikeOti <anigboguotuto@gmail.com>
2025-04-06 20:47:08 +0100

    added models and made migrations

--- Oti_Hospital_Rateapp/Treatement/models.py
+++ Oti_Hospital_Rateapp/Treatement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Prescription(models.Model):
+    drug = models.CharField(max_length=200)
+    doctor = models.CharField(max_length=100)
+    prescription_date = models.DateField()
+    dosage = models.IntegerField()
+
+    def __str__(self):
+        return self.doctor
