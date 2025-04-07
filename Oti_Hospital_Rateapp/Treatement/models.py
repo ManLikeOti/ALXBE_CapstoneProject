a2b52530d3cad1d42658834098fc5bd063ec861d
ManLikeOti <anigboguotuto@gmail.com>
2025-04-07 09:07:15 +0100

    updaed models code to implement datetime feature

--- Oti_Hospital_Rateapp/Treatement/models.py
+++ Oti_Hospital_Rateapp/Treatement/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
 # Create your models here.
 
 class Prescription(models.Model):
     drug = models.CharField(max_length=200)
     doctor = models.ForeignKey(User, on_delete=models.CASCADE)
-    prescription_date = models.DateField()
+    prescription_date = models.DateField(default=datetime.date.today)
     dosage = models.IntegerField()
 
     def __str__(self):
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 This is a repository for my ALX capstone project
+
+{
+        "username" : "Rainchecks",
+        "email" : "Rainchecks@example.com",
+        "password" : "12345passcheck",
+        "password2" : "12345passcheck",
+        "first_name" : "Rains",
+        "last_name" : "Checks"
+}
+
+
+{
+        "username" : "Rainchecks",
+        "password" : "12345passcheck"
+}
+
+{
+        "drug" : "antiviral",
+        "dosage" : "2"
+}
